321158290d9fc034bcc3dff5226e97bf29f4d7f3
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2024-02-17 15:22:41 +0530

    Add Redis command parser function

--- lib/server.ex
+++ lib/server.ex
@@ -14,9 +14,64 @@ defmodule Server do
     loop(client)
   end
 
+  def parse_array(data, count, acc \\ []) do
+    case count do
+      0 -> {%{type: :array, data: acc}, data}
+      _ ->
+        {parsed, rest} = parse(data)
+        parse_array(rest, count - 1, acc ++ [parsed])
+    end
+  end
+
+  def parse_bulk(data, count) do
+    case data do
+      [s | rest] ->
+        {%{type: :bulk, data: s}, rest}
+    end
+  end
+
+  def parse_simple(data) do
+    case data do
+      [s | rest] ->
+        {%{type: :simple, data: s}, rest}
+    end
+  end
+
+  @doc """
+  Parse the incoming redis command
+  """
+  def parse(data) do
+    # if starts with * then it's a multi-bulk request
+    # if starts with $ then it's a bulk request
+    # if starts with + then it's a status request
+    # for now, just parse these three types
+    regex = ~r/^(\*|\$|\+)(\d+)$/
+    case data do
+      # check if it starts with *
+      [s | rest] ->
+        case Regex.run(regex, s) do
+          [_, c, count] ->
+            IO.inspect c
+            case c do
+              "*" -> parse_array(rest, String.to_integer(count))
+              "$" -> parse_bulk(rest, String.to_integer(count))
+              "+" -> parse_simple(rest)
+            end
+          _ -> {:error, "Invalid request"}
+        end
+    end
+  end
+
+  def split_and_parse(data) do
+    parse(String.split(data, "\r\n"))
+  end
+
   def loop(client) do
     case :gen_tcp.recv(client, 0) do
-      {:ok, _data} ->
+      {:ok, data} ->
+        IO.puts "Received: #{data}"
+        parsed = split_and_parse(data)
+        IO.inspect parsed
         :gen_tcp.send(client, "+PONG\r\n")
         loop(client)
       {:error, :closed} ->
