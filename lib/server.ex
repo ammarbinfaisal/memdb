d27c205a797a8098a12d413a22bb8668b3e3097a
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2024-02-17 14:00:41 +0530

    send pong

--- lib/server.ex
+++ lib/server.ex
@@ -9,11 +9,38 @@ defmodule Server do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
+  def handle_client(client) do
+    IO.puts "Client connected"
+    loop(client)
+  end
+
+  def loop(client) do
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} ->
+        :gen_tcp.send(client, "+PONG\r\n")
+        loop(client)
+      {:error, :closed} ->
+        IO.puts "Client disconnected"
+    end
+  end
+
+  defp loop_acceptor(socket) do
+    case :gen_tcp.accept(socket) do
+      {:ok, client} ->
+        spawn(fn -> handle_client(client) end)
+        loop_acceptor(socket)
+      {:error, reason} ->
+        IO.puts "Error accepting connection: #{reason}"
+    end
+  end
+
   @doc """
   Listen for incoming connections
   """
   def listen() do
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+
+    # spawn a new process for each incoming connection
+    loop_acceptor(socket)
   end
 end
