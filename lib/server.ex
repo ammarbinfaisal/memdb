f02e19a4a2960a19630ed3bfa3a49016ccc51add
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2024-02-18 13:44:29 +0530

    .

--- lib/server.ex
+++ lib/server.ex
@@ -13,18 +13,21 @@ defmodule Server do
     # find --replicaof host port
     replicaof_host = args |> Enum.at(3)
     replicaof_port = args |> Enum.at(4)
+
     if replicaof_host && replicaof_port do
-      IO.puts "Starting as a slave"
+      IO.puts("Starting as a slave")
       listen(port, {replicaof_host, String.to_integer(replicaof_port)})
     else
-      IO.puts "Starting as a master"
+      IO.puts("Starting as a master")
       listen(port, nil)
     end
   end
 
   def parse_array(data, count, acc \\ []) do
     case count do
-      0 -> {{:array, acc}, data}
+      0 ->
+        {{:array, acc}, data}
+
       _ ->
         {parsed, rest} = parse(data)
         parse_array(rest, count - 1, acc ++ [parsed])
@@ -50,6 +53,7 @@ defmodule Server do
   """
   def parse(data) do
     regex = ~r/^(\*|\$|\+)(\d+)$/
+
     case data do
       # check if it starts with *
       [s | rest] ->
@@ -60,59 +64,79 @@ defmodule Server do
               "$" -> parse_bulk(rest, String.to_integer(count))
               "+" -> parse_simple(rest)
             end
-          _ -> {:error, "Invalid request"}
+
+          _ ->
+            {:error, "Invalid request"}
         end
     end
   end
 
   defp expire(key, ttl) do
     :timer.sleep(ttl)
-    IO.puts "Expiring key #{key}"
+    IO.puts("Expiring key #{key}")
     GenServer.call(KeyValue, {:delete, key})
   end
 
   def exec(data, client, info) do
     {parsed, _} = parse(String.split(data, "\r\n"))
     IO.inspect(parsed)
+
     case parsed do
       # handle echo and ping commands
       {:array, [bulk: "echo", bulk: s]} ->
         :gen_tcp.send(client, encode_bulk(s))
+
       {:array, [bulk: "ping"]} ->
         :gen_tcp.send(client, "+PONG\r\n")
+
       {:array, [bulk: "get", bulk: key]} ->
         value = GenServer.call(KeyValue, {:get, key})
+
         case value do
           nil -> :gen_tcp.send(client, "$-1\r\n")
           _ -> :gen_tcp.send(client, encode_bulk(value))
         end
+
       {:array, [bulk: "set", bulk: key, bulk: value]} ->
         GenServer.call(KeyValue, {:set, key, value})
         :gen_tcp.send(client, "+OK\r\n")
+
       {:array, [bulk: "set", bulk: key, bulk: value, bulk: "px", bulk: ttl]} ->
         GenServer.call(KeyValue, {:set, key, value})
         spawn(fn -> expire(key, String.to_integer(ttl)) end)
         :gen_tcp.send(client, "+OK\r\n")
+
       {:array, [bulk: "delete", bulk: key]} ->
         value = GenServer.call(KeyValue, {:delete, key})
         IO.inspect(value)
-        :gen_tcp.send(client,"+OK\r\n")
+        :gen_tcp.send(client, "+OK\r\n")
+
       {:array, [bulk: "info", bulk: "replication"]} ->
-        info = case info.role do
-          "master" -> "role:master\nmaster_replid:#{info.run_id}\nmaster_repl_offset:0\n"
-          "slave" -> "role:slave\nmaster_host:#{info.master_host}\nmaster_port:#{info.master_port}\n"
-        end
+        info =
+          case info.role do
+            "master" ->
+              "role:master\nmaster_replid:#{info.run_id}\nmaster_repl_offset:0\n"
+
+            "slave" ->
+              "role:slave\nmaster_host:#{info.master_host}\nmaster_port:#{info.master_port}\n"
+          end
+
         :gen_tcp.send(client, encode_bulk(info))
+
       {:array, [bulk: "replconf", bulk: "listening-port", bulk: port]} ->
-        IO.puts "Listening port set to #{port}"
+        IO.puts("Listening port set to #{port}")
         :gen_tcp.send(client, "+OK\r\n")
+
       {:array, [bulk: "replconf", bulk: "capa", bulk: "psync2"]} ->
-        IO.puts "PSYNC2 enabled"
+        IO.puts("PSYNC2 enabled")
         :gen_tcp.send(client, "+OK\r\n")
+
       {:array, [bulk: "psync", bulk: "?", bulk: "-1"]} ->
-        IO.puts "PSYNC requested"
+        IO.puts("PSYNC requested")
         :gen_tcp.send(client, "+OK\r\n")
-      _ -> :gen_tcp.send(client, "Invalid command\r\n")
+
+      _ ->
+        :gen_tcp.send(client, "Invalid command\r\n")
     end
   end
 
@@ -129,8 +153,9 @@ defmodule Server do
       {:ok, data} ->
         exec(data, client, master)
         loop(client, master)
+
       {:error, :closed} ->
-        IO.puts "Client disconnected"
+        IO.puts("Client disconnected")
     end
   end
 
@@ -139,8 +164,9 @@ defmodule Server do
       {:ok, client} ->
         spawn(fn -> loop(client, info) end)
         loop_acceptor(socket, info)
+
       {:error, reason} ->
-        IO.puts "Error accepting connection: #{reason}"
+        IO.puts("Error accepting connection: #{reason}")
     end
   end
 
@@ -152,41 +178,67 @@ defmodule Server do
 
     case res do
       {:ok, socket} ->
-        KeyValue.start_link
+        KeyValue.start_link()
         run_id = Base.encode16(:crypto.strong_rand_bytes(20))
-        info = case master do
-          nil -> %{
-            role: "master",
-            run_id: run_id
-          }
-          {master_host, master_port} ->
-            # handshake with master
-            opts = [:binary, :inet, active: false, packet: :line]
-            # convert ip string to tuple
-            ip = case master_host do
-              "localhost" -> {127, 0, 0, 1}
-              _ -> String.split(master_host, ".") |> Enum.map(&String.to_integer/1) |> List.to_tuple
-            end
-            {:ok, conn} = :gen_tcp.connect(ip, port, opts)
-            :gen_tcp.send(conn, encode_array([encode_bulk("ping")], 1))
-            IO.inspect :gen_tcp.recv(conn, 0)
-            :gen_tcp.send(conn, encode_array([encode_bulk("replconf"), encode_bulk("listening-port"), encode_bulk(Integer.to_string(port))], 3))
-            IO.inspect :gen_tcp.recv(conn, 0)
-            :gen_tcp.send(conn, encode_array([encode_bulk("replconf"), encode_bulk("capa"), encode_bulk("psync2")], 3))
-            IO.inspect :gen_tcp.recv(conn, 0)
-            :gen_tcp.send(conn, encode_array([encode_bulk("psync"), encode_bulk("?"), encode_bulk("-1")], 3))
-            IO.inspect :gen_tcp.recv(conn, 0)
-            :gen_tcp.clone(conn)
-            %{
-              role: "slave",
-              run_id: run_id,
-              master_host: master_host,
-              master_port: master_port
-            }
-        end
+
+        info =
+          case master do
+            nil ->
+              %{
+                role: "master",
+                run_id: run_id
+              }
+
+            {master_host, master_port} ->
+              opts = [:binary, :inet, active: false, packet: :line]
+
+              ip =
+                case master_host do
+                  "localhost" ->
+                    {127, 0, 0, 1}
+
+                  _ ->
+                    String.split(master_host, ".")
+                    |> Enum.map(&String.to_integer/1)
+                    |> List.to_tuple()
+                end
+
+              {:ok, conn} = :gen_tcp.connect(ip, port, opts)
+
+              :gen_tcp.send(conn, ["ping"] |> Enum.map(&encode_bulk/1) |> Enum.join())
+              IO.inspect(:gen_tcp.recv(conn, 0))
+
+              :gen_tcp.send(
+                conn,
+                ["replconf", "listening-port", Integer.to_string(port)]
+                |> Enum.map(&encode_bulk/1)
+                |> Enum.join()
+              )
+
+              IO.inspect(:gen_tcp.recv(conn, 0))
+
+              :gen_tcp.send(
+                conn,
+                ["replconf", "capa", "psync2"] |> Enum.map(&encode_bulk/1) |> Enum.join()
+              )
+
+              # :gen_tcp.send(conn, encode_array([encode_bulk("psync"), encode_bulk("?"), encode_bulk("-1")], 3))
+              :gen_tcp.send(conn, ["psync", "?", "-1"] |> Enum.map(&encode_bulk/1) |> Enum.join())
+              IO.inspect(:gen_tcp.recv(conn, 0))
+              :gen_tcp.close(conn)
+
+              %{
+                role: "slave",
+                run_id: run_id,
+                master_host: master_host,
+                master_port: master_port
+              }
+          end
+
         loop_acceptor(socket, info)
+
       {:error, err} ->
-        IO.inspect err
+        IO.inspect(err)
         System.halt(1)
     end
   end
